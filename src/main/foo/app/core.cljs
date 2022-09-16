187e43cee768e075ba1a069f63fe392e78b39969
Morten Emmanuel Schiøler <mortenschioler@gmail.com>
2022-09-16 12:52:39 +0000

    Start over with Reagent

---
+++ src/main/foo/app/core.cljs
@@ -0,0 +1,19 @@
+(ns foo.app.core
+  (:require [reagent.dom :as rdom]))
+
+(defn app
+  []
+  [:div
+   [:h1 "Squarity"]])
+
+(defn render 
+  []
+  (rdom/render [app] (.getElementById js/document "root")))
+
+(defn ^:export main 
+  []
+  (render))
+
+(defn ^:dev/after-load reload! 
+  []
+  (render))
---
+++ src/test/foo/app/core_test.cljs
@@ -0,0 +1,1 @@
+(ns foo.app.core-test)
