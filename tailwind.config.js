8f1894b3ef98e67661838ff22bbc8e22f091693c
Morten Emmanuel Schiøler <mortenschioler@gmail.com>
2022-09-16 20:07:07 +0000

    Introduce tailwind! 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/main/**/*.{html,js,cljs}", "public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
